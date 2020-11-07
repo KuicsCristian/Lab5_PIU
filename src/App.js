13c0d0269fd9ab7fb59e8c9b9b98bc2d9b9b67a3
KuicsCristian <47479919+KuicsCristian@users.noreply.github.com>
2020-11-07 11:13:38 +0200

    Lab5_6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import "./styles.css";
+import Impresie from "./impresie";
+import Container from "react-bootstrap/Container";
+import ListaImpresii from "./listaimpresii";
+import { alignPropType } from "react-bootstrap/esm/DropdownMenu";
 
+const lista = [
+  {
+    titlu: "Do not get any service if you want to cancel your subscription",
+    text: `Aww yeah, you successfully read this important alert message. This
+        example text is going to run a bit longer so that you can see how
+        spacing within an alert works with this kind of content.`,
+    id: 1
+  },
+  {
+    titlu: "Postare 2: Do not get any service",
+    text: `Aww yeah, you successfully read this important alert message. This
+      example text is going to run a bit longer so that you can see how
+      spacing within an alert works with this kind of content.`,
+    id: 2
+  }
+];
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Container>
+        <h1 style={{ textAlign: "center" }}>Lista de impresii</h1>
+        <ListaImpresii listaImpresii={lista} />
+      </Container>
     </div>
   );
 }
